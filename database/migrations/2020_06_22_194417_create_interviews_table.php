83478d2e05b6a3e5d0f6302fd1d7d491e0816ddb
eduruiz <contato@eduruiz.com.br>
2020-06-22 17:29:35 -0300

    Add migration to create interviews table

---
+++ database/migrations/2020_06_22_194417_create_interviews_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInterviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('interviews', function (Blueprint $table) {
+			$table->increments('id');
+			$table->enum('status', array('PUBLISHED','DRAFT','PENDING'))->default('DRAFT');
+			$table->string('title');
+			$table->string('slug', 250)->unique('interviews_slug_unique');
+			$table->text('excerpt', 65535)->nullable();
+			$table->text('body', 65535);
+			$table->string('image')->nullable();
+			$table->string('image_alt')->nullable();
+			$table->string('seo_title')->nullable();
+			$table->text('meta_description', 65535)->nullable();
+			$table->text('featured')->nullable();
+			$table->integer('author_id')->nullable();
+			$table->integer('category_id')->nullable();
+			$table->timestamps();
+			$table->integer('order')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('interviews');
+    }
+}
