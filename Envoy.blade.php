3f2c3fc1e4edac4baa44fa5fcdc1fb262d5dfac2
eduruiz <contato@eduruiz.com.br>
2020-09-11 20:37:50 -0300

    Update envoy config

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,18 +1,18 @@
 @setup
-require __DIR__.'/vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+  require __DIR__.'/vendor/autoload.php';
+  $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+  $dotenv->load();
 
-$homolServer = env('HOMOL_SERVER_ADDRESS')
+  $homolServer = env('HOMOL_SERVER_ADDRESS');
+  $homolPath = env('HOMOL_SERVER_PATH');
 @endsetup
 
 @servers(['homol' => $homolServer])
 
-
 @task('deployHomol', ['on' => 'homol'])
-    sudo su
-    cd /home/jenkins-data/dev-persistent/comunitas-www/src
-    git pull
-    docker exec --user=laradock laradock_workspace_1 php artisan migrate
-    docker exec --user=laradock laradock_workspace_1 php artisan cache:clear && php artisan view:clear
+  sudo su
+  cd {{ $homolPath }}
+  git pull
+  docker exec --user=laradock laradock_workspace_1 php artisan migrate
+  docker exec --user=laradock laradock_workspace_1 php artisan cache:clear && php artisan view:clear
 @endtask
